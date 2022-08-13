aede728448f39b2d912859287759a897c4e8bc6a
Imran Haider <imran@codesorbit.com>
2022-08-13 19:56:32 +0500

    changes

---
+++ src/shared/js/environment.js
@@ -0,0 +1,8 @@
+const url= 'https://art-fun.herokuapp.com/';
+// const url= 'http://localhost:8000/';
+const environment = {
+  baseUrl: `${url}api/`,
+  serverUrl:`${url}`
+};
+
+export default environment;
