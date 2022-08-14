0397a5ab13bacc4784ac2cf6912320d1d77a1445
Hamad Minhas <ihamadrajpoot@gmail.com>
2022-08-14 21:22:18 +0500

    arts/artist api integrated completely

--- src/screens/artist/artist.js
+++ src/screens/artist/artist.js
@@ -5,38 +5,40 @@ import {ScrollView} from 'react-native-gesture-handler';
 import ArtistCard from './artistCard';
 import { backendCall } from '../../shared/js/backendCall'
 import CustomButton from '../../shared/components/customButton';
-import GlobalStyles from '../../shared/styles/globalStyles';
+import { ActivityIndicator } from 'react-native-paper';
+import { showToastMessage } from '../../shared/js/showToastMessage';
 
 const Artist = () => {
   const [artistList, setArtistList] = useState([]);
   const [offSet, setOffSet] = useState(0);
+  const [loadMore, setLoadMore] = useState(false);
+  const [disable, setDisable] = useState(false);
 
   useEffect(() => {
     getArtists();
   },[offSet])
 
   const getArtists = async () => {
-    const response = await backendCall(`arts/artist/?pageSize=2&offset=${offSet}`,"GET");
+    const response = await backendCall(`arts/artist/?pageSize=1&offset=${offSet}`,"GET");
     if (response && response?.data?.artistList?.length){
       let artists = [];
-      if (artistList.length){
-        response.data.artistList.map((artist) => {
-          artists = [...artistList, artist];
-        })
-        setArtistList(artists);
-      }
-      else
-        setArtistList(response.data.artistList);
+      response.data.artistList.map((artist) => {
+        artists = [...artistList, artist];
+      })
+      setArtistList(artists);
     }
+    else if (response?.data?.artistList.length === 0){
+      showToastMessage("error","top", "No more records available");
+    }
+    setLoadMore(false);
   }
 
   console.log("ARTIST LIST",artistList);
   return (
-    <View style={{borderWidth: 5, borderColor: 'green', flex: 1}}>
+    <View style={[styles.mainWrapper]}>
       <Header title={'Artists'} />
-      <ScrollView style={{borderWidth: 5, borderColor: 'blue'}}
-      contentContainerStyle={{
-        alignItems: 'center'
+      <ScrollView style={{
+        // borderWidth: 5, borderColor: 'blue'
       }}
       >
         <View style={[styles.container]}>
@@ -44,33 +46,46 @@ const Artist = () => {
             return <ArtistCard key={index} data={artist} />;
           })}
         </View>
+        {
+          artistList.length > 0 ?
+          loadMore ?
+          <ActivityIndicator />
+          :
+          <CustomButton
+            Title="View More"
+            contentStyle={[styles.viewMoreBtnWrapper]}
+            IsLoading={false}
+            onPress={() => {
+              setLoadMore(true);
+              setOffSet(offSet + 1)
+            }}
+          /> : null
+        }
       </ScrollView>
-      <CustomButton
-        Title="View More"
-        // style={[GlobalStyles.mt3]}
-        contentStyle={[styles.viewMoreBtnWrapper]}
-        IsLoading={false}
-        onPress={() => { setOffSet(offSet + 1) }}
-      />
     </View>
   );
 };
 export default Artist;
 
 const styles = StyleSheet.create({
+  mainWrapper : {
+    flex: 1,
+    // borderWidth: 5,
+    // borderColor: 'green'
+  },
   container: {
     flexDirection: 'row',
     padding: 15,
     flexWrap: 'wrap',
     // marginBottom: 70,
     justifyContent: 'space-between',
-    borderWidth: 2,
-    borderColor: 'red'
+    // borderWidth: 5,
+    // borderColor: 'red'
   },
   viewMoreBtnWrapper : {
     width : '50%',
-    marginTop: 20,
-    marginBottom: 10,
+    // marginTop: 20,
+    marginBottom: 50,
     alignSelf: 'center'
   }
 });
